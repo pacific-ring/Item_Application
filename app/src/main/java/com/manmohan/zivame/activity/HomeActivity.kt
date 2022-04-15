91b4692c02e82ce50a5aa9585daebd711c5e6d95
manmohangupta <manmohan.gupta@paytm.com>
2022-04-16 02:46:14 +0530

    Manmohan : 1. As Per Required Moved to Data Binding.

--- app/src/main/java/com/manmohan/zivame/activity/HomeActivity.kt
+++ app/src/main/java/com/manmohan/zivame/activity/HomeActivity.kt
@@ -3,9 +3,11 @@ package com.manmohan.zivame.activity
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.manmohan.zivame.R
+import com.manmohan.zivame.databinding.ActivityMainBinding
 import com.manmohan.zivame.di.components.DaggerHomeScreenActivityComponent
 import com.manmohan.zivame.di.components.HomeScreenActivityComponent
 import com.manmohan.zivame.viewmodel.HomeViewModel
@@ -16,14 +18,14 @@ import javax.inject.Inject
 class HomeActivity : AppCompatActivity() {
     lateinit var homeScreenActivityComponent: HomeScreenActivityComponent
     lateinit var homeActivityViewModel: HomeViewModel
+    lateinit var binding : ActivityMainBinding
 
     @Inject
     lateinit var homeActivityViewModelFactory: HomeViewModelFactory
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         initialize()
         initializeViewModel()
         fetchProductData()
